42e5f38e1783006f80ad0e666141d6360c5f905a
mbagnsk <56918406+mbagnsk@users.noreply.github.com>
2021-05-06 00:22:48 +0200

    ESP MQTT Publisher
    Created a ESP MQTT Publisher program. Not tried it live yet. Configuration file.

---
+++ esp mqtt/esp_publisher.ino
@@ -0,0 +1,85 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid ="TP-Link_7AF2"; //replace this with your wifi  name
+const char* password ="baginski1"; //replace with your wifi password
+char hostname[] ="192.168.1.27"; //replace this with IP address of machine 
+//on which broker is installed
+#define TOKEN "bytesofgigabytes"
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+int status = WL_IDLE_STATUS;
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+  delay(500);
+  Serial.print(".");
+  }
+  Serial.println("Connected to WiFi");
+  Serial.println("ESP8266 AS PUBLISHER");
+  client.setServer(hostname, 1883 ); //default port for mqtt is 1883
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  if ( !client.connected())
+  {
+    reconnect();
+  }
+  MQTTPOST();
+  delay(5000);//delay 5 Sec
+}
+
+void MQTTPOST()
+{
+  //payload formation begins here
+  String payload ="{";
+  payload +="\"Temp\":"; payload +=10; payload +=",";
+  payload +="\"Humi\":"; payload +=20;
+  payload +="}";
+
+  char attributes[1000];
+  payload.toCharArray( attributes, 1000 );
+  client.publish("test", attributes); //topic="test" MQTT data post command.
+  Serial.println( attributes );
+}
+
+//this function helps you reconnect wifi as well as broker if connection gets disconnected.
+void reconnect() 
+{
+  while (!client.connected())
+  {
+    status = WiFi.status();
+    if ( status != WL_CONNECTED) 
+    {
+      WiFi.begin(ssid, password);
+      while (WiFi.status() != WL_CONNECTED) 
+      {
+        delay(500);
+        Serial.print(".");
+      }
+    Serial.println("Connected to AP");
+    }
+    Serial.print("Connecting to Broker …");
+    Serial.print("192.168.43.220");
+  
+    if ( client.connect("ESP8266 Device", TOKEN, NULL) )
+    {
+      Serial.println("[DONE]" );
+    }
+    else 
+    {
+    Serial.println( " : retrying in 5 seconds]" );
+    delay( 5000 );
+    }
+  }
+}
