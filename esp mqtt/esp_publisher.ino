5ce62a73b4102bed8218876e3778541ae7db4589
mbagnsk <56918406+mbagnsk@users.noreply.github.com>
2021-05-24 02:15:01 +0200

    Publisher that sends a correct data to established topics

--- esp mqtt/esp_publisher.ino
+++ esp mqtt/esp_publisher.ino
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ESPDateTime.h>
 
 const char* ssid ="TP-Link_7AF2"; //replace this with your wifi  name
 const char* password ="baginski1"; //replace with your wifi password
@@ -11,6 +12,7 @@ WiFiClient wifiClient;
 PubSubClient client(wifiClient);
 
 int status = WL_IDLE_STATUS;
+unsigned long timeSinceStart;
 
 void setup() 
 {
@@ -35,21 +37,60 @@ void loop()
   {
     reconnect();
   }
-  MQTTPOST();
-  delay(5000);//delay 5 Sec
+  MQTTPOST_temperature();
+  delay(2000);
+  MQTTPOST_alarm();
+  delay(2000);
+  MQTTPOST_lighting();
+  delay(500000);//delay 5 Sec // 500 secs
 }
 
-void MQTTPOST()
+void MQTTPOST_temperature()
 {
   //payload formation begins here
-  String payload ="{";
-  payload +="\"Temp\":"; payload +=10; payload +=",";
-  payload +="\"Humi\":"; payload +=20;
-  payload +="}";
+  String payload = "temperature"; // topic
+  payload += ",1"; // id_device 
+  payload +=",1,"; // id_channel
+  payload += DateTime.formatUTC("%Y-%m-%d %H:%M:%S");
+  payload += ",36"; // measurement
+  payload += ",temperature"; // measurement type
+  payload += ",C"; // measurement units, only for temperature
 
   char attributes[1000];
   payload.toCharArray( attributes, 1000 );
-  client.publish("test", attributes); //topic="test" MQTT data post command.
+  client.publish("temperature", attributes); //topic="test" MQTT data post command.
+  Serial.println( attributes );
+}
+
+void MQTTPOST_alarm()
+{
+  //payload formation begins here
+  String payload = "alarm"; // topic
+  payload += ",2"; // id_device 
+  payload +=",1,"; // id_channel
+  payload += DateTime.formatUTC("%Y-%m-%d %H:%M:%S");
+  payload += ",1"; // measurement
+  payload += ",alarm"; // measurement type 
+
+  char attributes[1000];
+  payload.toCharArray( attributes, 1000 );
+  client.publish("alarm", attributes); //topic="test" MQTT data post command.
+  Serial.println( attributes );
+}
+
+void MQTTPOST_lighting()
+{
+  //payload formation begins here
+  String payload = "lighting"; // topic
+  payload += ",3"; // id_device
+  payload +=",1,"; // id_channel
+  payload += DateTime.formatUTC("%Y-%m-%d %H:%M:%S");
+  payload += ",0"; // measurement
+  payload += ",lighting"; // measurement type
+
+  char attributes[1000];
+  payload.toCharArray( attributes, 1000 );
+  client.publish("lighting", attributes); //topic="test" MQTT data post command.
   Serial.println( attributes );
 }
 
